a2672b91a74e31682de2291119d071f512c661cc
Vladislav Oboturov <vlad100493@gmail.com>
2023-09-01 20:13:30 +0300

    1

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/coffee-studio-test/'
-    : '/'
+  transpileDependencies: true
 })
